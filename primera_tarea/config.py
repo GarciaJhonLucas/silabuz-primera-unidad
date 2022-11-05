0ea844660c38013d7ae8b92bccc51f90cf32171c
Bryan Ochoa <bochoatrillo@gmail.com>
2022-11-05 17:30:01 -0500

    Crea metodos para imprimir mensajes y mejorar la interaccion

--- primera_tarea/config.py
+++ primera_tarea/config.py
@@ -7,3 +7,18 @@ def limpiar_consola():
     if os.name in ('nt', 'dos'):  # Si el SO es Windows, cambia a 'cls'
         command = 'cls'
     os.system(command)
+    
+def mensaje_opcion(mensaje:str, salida: int)-> str:
+    valor=""
+    while True:
+        valor = input(Fore.CYAN+mensaje+' !# '+Fore.RESET)
+        if valor != '' and valor.isnumeric() and int(valor) < salida + 1:
+            break
+        imprimir_errores('Opcion no es valida, Ingrese un numero')
+    return valor
+
+def imprimir_errores(mensaje:str)->None:
+    print(Fore.RED+'[Error]'+Fore.RESET+' '+mensaje )
+
+def imprimir_informacion()->None:
+    input(Fore.GREEN+'[Mensaje]'+Fore.RESET+' Precione Enter para continuar')
